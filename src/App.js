ea902cd95cc0e7227245589f3ef878677843677a
sammy <sammylaureys@hotmail.com>
2020-10-17 15:59:37 +0200

    small app done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hej vandaag ben ik naar de {process.env.REACT_APP_INITIAL_TEXT} geweest.</p>
       </header>
     </div>
   );
