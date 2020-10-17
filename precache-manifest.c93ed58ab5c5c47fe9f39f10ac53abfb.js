f7d688293598883920c8da25972a8b285972f471
sammy <sammylaureys@hotmail.com>
2020-10-17 16:00:59 +0200

    Updates

---
+++ precache-manifest.c93ed58ab5c5c47fe9f39f10ac53abfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8306e2ea71a8184fe53262000d0ff86e",
+    "url": "/testing_small_react_app/index.html"
+  },
+  {
+    "revision": "f2194c3e110e5e83338a",
+    "url": "/testing_small_react_app/static/css/main.7715da5c.chunk.css"
+  },
+  {
+    "revision": "65906ab4ca972b84ffcb",
+    "url": "/testing_small_react_app/static/js/2.a7d4e85b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/testing_small_react_app/static/js/2.a7d4e85b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2194c3e110e5e83338a",
+    "url": "/testing_small_react_app/static/js/main.fa3097dd.chunk.js"
+  },
+  {
+    "revision": "b4ee8fc84ca85bbdf1b3",
+    "url": "/testing_small_react_app/static/js/runtime-main.866d9e95.js"
+  }
+]);
